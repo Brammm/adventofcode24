a956597baa149a861865c0dad59902dd36ee2774
Bram Van der Sype <bram.vandersype@gmail.com>
2024-12-02 16:40:29 +0100

    day 2 part 2 still incorrect, too low now :(

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,5 @@
 pub mod day2 {
+    use std::cmp::max;
     use std::fs;
 
     pub fn run() {
@@ -14,7 +15,10 @@ pub mod day2 {
             .collect();
 
         println!("Total valid reports is: {}", count_valid_reports(&reports));
-        println!("Total kinda valid reports is: {}", count_kinda_valid_reports(&reports));
+        println!(
+            "Total kinda valid reports is: {}",
+            count_kinda_valid_reports(&reports)
+        );
     }
 
     fn count_valid_reports(reports: &Vec<Vec<i32>>) -> i32 {
@@ -26,46 +30,45 @@ pub mod day2 {
     }
 
     fn is_safe(data: &Vec<i32>, tolerance: i32) -> bool {
-        let pairs = data.len() - (1 + tolerance as usize);
-        let decreasing_count = get_decreasing_pairs_count(data);
-        let increasing_count = get_increasing_pairs_count(data);
+        let tolerance = tolerance;
 
-        (decreasing_count >= pairs || increasing_count >= pairs) && is_within_limits(data, tolerance)
+        let pairs = data.len() - 1;
+        let count = get_increasing_decreasing_pairs_count(data);
+
+        let difference = (pairs - count) as i32;
+
+        difference <= tolerance && is_within_limits(data, max(0, tolerance - difference))
     }
 
-    fn get_decreasing_pairs_count(data: &Vec<i32>) -> usize {
-        data.windows(2).filter(|pair| pair[0] > pair[1]).count()
-    }
-
-    fn get_increasing_pairs_count(data: &Vec<i32>) -> usize {
-        data.windows(2).filter(|pair| pair[0] < pair[1]).count()
+    fn get_increasing_decreasing_pairs_count(data: &Vec<i32>) -> usize {
+        max(
+            data.windows(2).filter(|pair| pair[0] >= pair[1]).count(),
+            data.windows(2).filter(|pair| pair[0] <= pair[1]).count(),
+        )
     }
 
     fn is_within_limits(data: &Vec<i32>, tolerance: i32) -> bool {
-        // piets eerste element van den array (als da faalt bv array niet lang genoeg, return false)
         if let Some((first, rest)) = data.split_first() {
             let mut previous = first;
             let mut error_count = 0;
-    
-            // loop over de rest van de items, vergelijkend met "previous", eerste loop dus eerste element
+
             for value in rest {
-                let diff = (value - previous).abs(); // bereken absolute verschil
-    
-                if ![1, 2, 3].contains(&diff) { // difference is kleiner dan 1, groter dan 3
-                    if error_count == tolerance { // als we aan onze tolerance zitten, false
+                let diff = (value - previous).abs();
+                if ![1, 2, 3].contains(&diff) {
+                    if error_count == tolerance {
                         return false;
                     } else {
-                        error_count += 1; // nog niet aan onze tolerance, verhoog error count en vergelijk met volgend nummer
+                        error_count += 1;
                         continue;
                     }
                 }
-    
-                previous = value; // boel is nog geldig dus overwrite previous
+
+                previous = value;
             }
-    
+
             return true;
         }
-    
+
         false
     }
 
@@ -73,19 +76,20 @@ pub mod day2 {
     mod tests {
         use super::*;
 
-        static TEST_DATA: [[i32; 5]; 6] = [
+        static TEST_DATA: [[i32; 5]; 7] = [
             [9, 6, 4, 2, 1],
             [1, 2, 7, 8, 9],
             [9, 7, 6, 2, 1],
             [1, 3, 2, 4, 5],
             [8, 6, 4, 4, 1],
             [1, 3, 6, 7, 9],
+            [1, 3, 2, 5, 12],
         ];
 
         #[test]
         fn test_is_perfectly_safe() {
-            let expected = vec![true, false, false, false, false, true];
-        
+            let expected = vec![true, false, false, false, false, true, false];
+
             for (index, expected) in expected.iter().enumerate() {
                 assert_eq!(
                     is_safe(&TEST_DATA[index].to_vec(), 0),
@@ -98,7 +102,7 @@ pub mod day2 {
 
         #[test]
         fn test_is_kinda_safe() {
-            let expected = vec![true, false, false, true, true, true];
+            let expected = vec![true, false, false, true, true, true, false];
 
             for (index, expected) in expected.iter().enumerate() {
                 assert_eq!(
